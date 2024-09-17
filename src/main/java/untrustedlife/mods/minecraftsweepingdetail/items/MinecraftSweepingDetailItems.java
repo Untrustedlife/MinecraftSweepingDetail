a47ced7aaf867ee32d3e8862951981fb1f2a7a58
Untrustedlife <bountybayonlineroblox@gmail.com>
2024-09-17 12:51:42 -0500

    Working towards adding all my items

--- src/main/java/untrustedlife/mods/minecraftsweepingdetail/items/MinecraftSweepingDetailItems.java
+++ src/main/java/untrustedlife/mods/minecraftsweepingdetail/items/MinecraftSweepingDetailItems.java
@@ -1,9 +1,11 @@
 package untrustedlife.mods.minecraftsweepingdetail.items;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import java.util.List;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,11 +24,37 @@ public class MinecraftSweepingDetailItems {
     // Set up the DeferredRegister for items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MinecraftSweepingDetail.MODID);
     // Register your custom broom item
+
+
+    /*Actual Brooms */
     public static final RegistryObject<Item> BROOM = ITEMS.register("straw_broom",
         () -> new BroomItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB).durability(100),200,20,0));
-
     public static final RegistryObject<Item> BUNDLESTICKS = ITEMS.register("bundle_of_sticks",
         () -> new BroomItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB).durability(15),350,30,-2));
+
+    /*Useful Items */
+    public static final RegistryObject<Item> EMERALD_SHARD = ITEMS.register("emerald_shard",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB)));
+
+    public static final RegistryObject<Item> DIAMOND_SHARD = ITEMS.register("diamond_piece",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB)));
+    
+    public static final RegistryObject<Item> CLEANING_SHARD = ITEMS.register("cleaning_shard",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB)));
+
+    public static final RegistryObject<Item> CLEANING_EMERALD = ITEMS.register("cleaning_emerald",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB)));
+
+    public static final RegistryObject<Item> FROST_ROOT = ITEMS.register("frost_root",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB), 3, 0.7f, false, List.of(
+        new GarbageEffectData(MobEffects.MOVEMENT_SPEED, 400, 1, 1f)
+    )));
+
+    public static final RegistryObject<Item> FROST_BERRY = ITEMS.register("frost_berry",
+    () -> new BaseGarbageItem(new Item.Properties().tab(MinecraftSweepingDetailItems.SWEEPING_TAB), 3, 0.8f, false, List.of(
+        new GarbageEffectData(MobEffects.SATURATION, 4, 0, 1f)
+    )));
+
     // Create a custom Creative Mode Tab
     public static final CreativeModeTab SWEEPING_TAB = new CreativeModeTab("sweepingtab") {
         @Override
